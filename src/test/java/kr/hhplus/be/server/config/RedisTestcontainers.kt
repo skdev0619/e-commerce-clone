301d9393fe11edc499e3b6aacda0d27e957493d7
skdev0619 <skdev0619@naver.com>
2025-05-09 03:17:56 +0900

    [CHORE] redis 환경 설정 추가

---
+++ src/test/java/kr/hhplus/be/server/config/RedisTestcontainers.kt
@@ -0,0 +1,28 @@
+package kr.hhplus.be.server.config
+
+import com.redis.testcontainers.RedisContainer
+import jakarta.annotation.PreDestroy
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class RedisTestcontainers {
+
+    @PreDestroy
+    fun preDestroy() {
+        if (redisContainer.isRunning) redisContainer.stop()
+    }
+
+    companion object {
+
+        private val redisContainer = RedisContainer("redis:latest")
+            .apply {
+                start()
+            }
+
+        init {
+            System.setProperty("spring.data.redis.database", "0")
+            System.setProperty("spring.data.redis.master.host", redisContainer.host)
+            System.setProperty("spring.data.redis.master.port", redisContainer.firstMappedPort.toString())
+        }
+    }
+}
