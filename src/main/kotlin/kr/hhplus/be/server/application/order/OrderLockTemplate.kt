055e48318b928d5aa9008baf9f1134b2a3087100
skdev0619 <skdev0619@naver.com>
2025-05-09 03:46:46 +0900

    [FEAT] 주문 파사드에 상품 id별 스핀락 추가

---
+++ src/main/kotlin/kr/hhplus/be/server/application/order/OrderLockTemplate.kt
@@ -0,0 +1,30 @@
+package kr.hhplus.be.server.application.order
+
+import kr.hhplus.be.server.application.lock.DistributedLockManager
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.stereotype.Service
+import java.util.concurrent.TimeUnit
+
+@Service
+class OrderLockTemplate(
+    @Qualifier("lettuceDistributedLockManager")
+    private val lockManager: DistributedLockManager,
+    private val orderFacade: OrderFacade
+) {
+    companion object {
+        private const val DEFAULT_WAIT_TIME_MILLIS = 70000L   // 락을 시도할 최대 대기 시간
+        private const val DEFAULT_LEASE_TIME_MILLIS = 60000L  // 락 점유 시간 (TTL)
+    }
+
+    fun createOrder(criteria: OrderCriteria): OrderCompletedResult {
+        val lockKeys = criteria.toProductQuantities()
+            .map { "product-stoc-:${it.productId}" }
+
+        return lockManager.executeWithLocks(
+            lockKeys,
+            DEFAULT_WAIT_TIME_MILLIS,
+            DEFAULT_LEASE_TIME_MILLIS,
+            TimeUnit.MILLISECONDS
+        ) { orderFacade.createOrder(criteria) }
+    }
+}
